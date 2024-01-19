dd1aefc0f093f3636e55d9feff003048efa20702
The Perfect Name <52989569+fantashi099@users.noreply.github.com>
2024-01-19 16:14:22 +0700

    Update data_utils.py

--- deepcell/utils/data_utils.py
+++ deepcell/utils/data_utils.py
@@ -29,7 +29,7 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
 from tensorflow.keras import backend as K
-from keras.utils import conv_utils
+from tensorflow.python.keras.utils import conv_utils
 
 from deepcell.utils.tracking_utils import load_trks
 
